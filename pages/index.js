0b3ca0b8c14edc433453ae229c973b90c391c435
Axel Obscura <axosar@gmail.com>
2020-04-14 12:56:11 -0500

    Getting the env web

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 console.log('process: ' + JSON.stringify(process.env));
-console.log('process: ' + process.env.BACKEND_URL);
+console.log('process: ' + process.BACKEND_URL);
 
 export default () => (
   <div>Hello World. <Link href='/about' as={process.env.BACKEND_URL + '/about'}><a>About</a></Link></div>
